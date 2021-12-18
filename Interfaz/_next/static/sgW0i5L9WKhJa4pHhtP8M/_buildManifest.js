716ee2d30a626457e5713b5a4dc098d16b5bc072
--replace-all <Andre>
2021-12-18 13:47:00 -0600

    vacaciones

---
+++ Interfaz/_next/static/sgW0i5L9WKhJa4pHhtP8M/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/driver":["static/chunks/pages/driver-b146d924551bdc6e.js"],"/user":["static/chunks/pages/user-a711f8c3efeb585d.js"],sortedPages:["/_app","/_error","/driver","/user"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
